24031474e94192df0f250b8e36b7a5a328bd53f9
tetsu123 <perfect_blue@windowslive.com>
2015-04-23 16:49:53 +0700

    add Name.c

--- Name.c
+++ Name.c
@@ -4,10 +4,6 @@
 int main()
 {
 	printf("*********************\n");
-<<<<<<< HEAD
-	printf("Mink\n");
-=======
-	printf("Ball\n");
->>>>>>> e592d238f82ed3f5f8a5169071934e7af0e6fffe
+	printf("Max\n");
 	printf("*********************\n");
 }
