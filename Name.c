6c2bda51ec9f759eefc696d3627da5eb940e16a5
tetsu123 <perfect_blue@windowslive.com>
2015-04-23 18:22:34 +0700

    add *

--- Name.c
+++ Name.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-
+/*From User A*/
 int main()
 {
-	printf("**********************2\n");
-	printf("Ann22\n");
 	printf("**********************1\n");
 	printf("Ann11\n");
 	printf("**********************\n");
